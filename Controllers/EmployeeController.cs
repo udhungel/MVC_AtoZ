7b35be8755b553c944bb254f35d728d431577715
udhungel <udeepdhungel@gmail.com>
2022-11-18 18:58:58 +0100

    Part 4 Adding a Model in MVC to show it in View using  VierData

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using MVC_AtoZ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_AtoZ.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee emp = new Employee()
+                        {   
+                            EmployeeId = 1
+                           ,City = "Philly"
+                           ,Gender = "Male"
+                           ,Name = "Joe" 
+                        };
+
+            return View(emp);
+        }
+    }
+}
