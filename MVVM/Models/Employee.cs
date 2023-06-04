6d8eb71271dbe9f65350bdc20a59b2a2e3766813
udhungel <udeepdhungel@gmail.com>
2023-06-04 20:46:28 +0200

    Initial Config changes Added Empty Project Also a Coupon Web API project together with its model & AppDbContext

---
+++ MVVM/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
+
+namespace MVVM.Models
+{
+    public class Employee :INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged !=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return Name; }
+            set { Name = value; OnPropertyChanged("Name");}
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return Age; }
+            set { Age = value; OnPropertyChanged("Age");}
+        }
+
+
+
+    }
+}
---
+++ MVVM/Models/EmployeeService.cs
@@ -0,0 +1,72 @@
+﻿using DevExpress.Utils.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Models
+{
+    public  class EmployeeService
+    {
+        private static List<Employee> employeesobj;    
+        public EmployeeService()
+        {
+            employeesobj = new List<Employee>();
+            {
+                new Employee() { Id = 1, Age = 25, Name = "Joe" }; 
+            }
+
+        }
+
+        public List<Employee> getAll()
+        {
+            return employeesobj;
+        }
+
+        public bool Add(Employee employee) 
+        {
+            if (employee.Age > 21 || employee.Age > 58)
+                    throw new ArgumentException("Ivalid age limit for employee");
+           employeesobj.Add(employee);
+            return true;
+        }
+
+        public bool Delete(int Id) 
+        {
+            bool isDeleted = false;
+            var itemToDelete = employeesobj.Where(x=>x.Id == Id).FirstOrDefault();
+            if (itemToDelete !=null)
+            {
+                employeesobj.Remove(itemToDelete);
+                isDeleted = true;
+
+            }
+            return isDeleted; 
+        }
+
+        public bool Update(Employee employeetoUpdate )
+        {
+            bool isUpdated = false;
+            var empToUpdate = employeesobj.Where(x => x.Id == employeetoUpdate.Id).FirstOrDefault();
+            if (empToUpdate != null)
+            {
+                employeesobj.Remove(empToUpdate);
+                employeesobj.Add(employeetoUpdate);
+                isUpdated = true; 
+            }
+
+            return isUpdated; 
+        }
+
+        public Employee Search(int id)
+        {
+           var result =  employeesobj.Where(x => x.Id == id).FirstOrDefault();
+            if (result !=null )
+            {
+                return result;
+            }
+            return null;
+        }        
+    }
+}
